5c008171fd47b7710ec8be92b2d85444e251669f
takuyayamada <39893033+0105takuyayamada@users.noreply.github.com>
2019-05-20 21:00:31 +0900

    Add files via upload

---
+++ htdocs/index.php
@@ -0,0 +1,48 @@
+<?php
+session_start();
+
+$canselMessage = "";
+
+    //学生でなければ弾く
+if (!isset($_SESSION['USER'])) {
+    header('Location: login.php');
+    exit;
+}
+
+//ログアウト機能
+if(isset($_POST['logout'])){
+    
+    $_SESSION = [];
+    session_destroy();
+    header('Location: login.php');
+    exit;
+}
+?>
+ 
+<!DOCTYPE html>
+<html>
+<head>
+    <title>トップページ</title>
+</head>
+ 
+<body>
+<h1>弁当事前予約サービス</h1>
+<h2>トップページ</h2>
+<p>学生番号：<?php echo $_SESSION['USER'] ?>　ログイン中</p>
+
+<?php
+//キャンセル文
+if(!empty($_GET['canselName']))
+    echo "<p style='color:red;'>". $_GET['canselName'] ."の予約をキャンセルしました。<p>";
+?>
+
+<input type="button" onclick="location.href='./check.php'"value="予約確認"><br>
+<input type="button" onclick="location.href='リンク先url'"value="弁当予約"><br>
+<input type="button" onclick="location.href='./QRdisplay.php'"value="QRコード表示"><br>
+<br>
+<form method="post" action="top.php">
+    <input type="submit" name="logout" value="ログアウト">
+</form>
+ 
+</body>
+</html>
---
+++ htdocs/login.php
@@ -0,0 +1,73 @@
+<?php
+    session_start();
+ 
+    // 変数の初期化
+    $sql = null;
+    $result = null;
+    $db = null;
+    $message = '';
+
+    //ログイン済み
+    if (isset($_SESSION['USER'])) {
+        header('Location: index.php');
+        exit;
+    } else if (isset($_SESSION['VENDER'])) {
+        header('Location: vendorMenu.php');
+        exit;
+    }
+
+    //ログイン機能
+    if(!empty($_POST['login']) && !empty($_POST["student"]) && !empty($_POST["password"])){
+        
+        $student = $_POST["student"];
+        $password = $_POST["password"];
+
+        try {
+            //DBに接続
+            $db = new PDO("mysql:host=127.0.0.1; dbname=yabukib; charset=utf8", 'test', 'pass');    
+            //SQL作成・実行
+            $sql = 'SELECT * FROM studentLoginTable WHERE student = '. $student;
+            $prepare = $db->prepare($sql);
+            $prepare->execute();
+            $result = $prepare->fetch(PDO::FETCH_ASSOC);
+        
+        } catch(PDOException $e) {
+            echo $e->getMessage();
+            die();
+        }
+
+        //本人確認
+        if ($password == $result['password'] && $result['resumeDate'] == null) 
+        {
+            $_SESSION["USER"] = $_POST["student"];
+            header("Location: index.php");
+            exit;
+        }
+        else if ($result['resumeDate'] != null) {
+            $message = 'ペナルティがあるため、'.$result['resumeDate'].'　を過ぎるまでご利用いただけません。';
+        }
+    }
+?>
+ 
+<!DOCTYPE html>
+<html>
+<head>
+    <title>ログインページ</title>
+</head>
+ 
+<body>
+<h1>弁当事前予約サービス</h1>
+<h2>ログインページ</h2>
+<p style="color: red"><?php echo $message ?></p>
+<form method="post" action="login.php">
+    <label for="student">学生番号</label>
+    <input id="student" type="text" name="student">
+    <br>
+    <label for="password">パスワード</label>
+    <input id="password" type="password" name="password">
+    <br>
+    <input type="submit" name="login" value="ログイン">
+</form>
+ 
+</body>
+</html>
