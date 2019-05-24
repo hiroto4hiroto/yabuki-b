6b62558e05b80dd7d4ab42dd1fdd11ab8a398cde
takuyayamada <39893033+0105takuyayamada@users.noreply.github.com>
2019-05-24 16:33:24 +0900

    Update bentoList.php

--- htdocs/bentoList.php
+++ htdocs/bentoList.php
@@ -25,7 +25,7 @@ if (!isset($_SESSION['USER'])) {
 <p>弁当事前予約サービス</p>
 <h1>弁当閲覧・予約</h1>
     
-<table style="width: 80vw; height: 30vh;">
+<table style="width: 80vw;">
     <tr style="width: 100%; height: 5vh;">
         <td style="width: 70%; height: 100%;">弁当名
         <td style="width: 70%; height: 100%;">値段
