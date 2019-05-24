815c90c6214c095cf299f72f6723143024ab4f9d
takuyayamada <39893033+0105takuyayamada@users.noreply.github.com>
2019-05-24 15:34:54 +0900

    Add files via upload

---
+++ htdocs/bentoList.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+$canselMessage = "";
+    //学生でなければ弾く
+if (!isset($_SESSION['USER'])) {
+    header('Location: login.php');
+    exit;
+}
+?>
+ 
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <title>弁当事前予約サービス トップページ</title>
+     <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+ 
+<body>
+<p>弁当事前予約サービス</p>
+<h1>トップページ</h1>
+<p>学生番号：<?php echo $_SESSION['USER'] ?>　ログイン中</p>
+
+<?php
+//キャンセル文
+if(!empty($_GET['canselName']))
+    echo "<p style='color:red;'>". $_GET['canselName'] ."の予約をキャンセルしました。<p>";
+?>
+
+<input type="button" class="btn-sticky" onclick="location.href='./check.php'" value="予約確認"><br>
+<input type="button" class="btn-sticky" onclick="location.href='リンク先url'" value="弁当閲覧・予約" style="height: calc(var(--fontRatio) * 7.5);"><br>
+<input type="button" class="btn-sticky" onclick="location.href='./QRdisplay.php'" value="QRコード表示" style="height: calc(var(--fontRatio) * 7.5);"><br>
+<br>
+<form method="post" action="index.php">
+    <input type="submit" class="btn-sticky" name="logout" value="ログアウト">
+</form>
+ 
+</body>
+</html>
