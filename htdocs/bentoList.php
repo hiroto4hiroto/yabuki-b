c79b7cfb06d945ccf6164c9c4b1ff7a2d3d8a4b1
takuyayamada <39893033+0105takuyayamada@users.noreply.github.com>
2019-05-24 15:41:06 +0900

    Update bentoList.php

--- htdocs/bentoList.php
+++ htdocs/bentoList.php
@@ -12,28 +12,18 @@ if (!isset($_SESSION['USER'])) {
 <html>
 <head>
     <meta charset="utf-8"/>
-    <title>弁当事前予約サービス トップページ</title>
+    <title>弁当事前予約サービス 弁当閲覧・予約</title>
      <link rel="stylesheet" type="text/css" href="style.css">
 </head>
  
 <body>
 <p>弁当事前予約サービス</p>
-<h1>トップページ</h1>
-<p>学生番号：<?php echo $_SESSION['USER'] ?>　ログイン中</p>
-
-<?php
-//キャンセル文
-if(!empty($_GET['canselName']))
-    echo "<p style='color:red;'>". $_GET['canselName'] ."の予約をキャンセルしました。<p>";
-?>
-
-<input type="button" class="btn-sticky" onclick="location.href='./check.php'" value="予約確認"><br>
-<input type="button" class="btn-sticky" onclick="location.href='リンク先url'" value="弁当閲覧・予約" style="height: calc(var(--fontRatio) * 7.5);"><br>
-<input type="button" class="btn-sticky" onclick="location.href='./QRdisplay.php'" value="QRコード表示" style="height: calc(var(--fontRatio) * 7.5);"><br>
-<br>
-<form method="post" action="index.php">
-    <input type="submit" class="btn-sticky" name="logout" value="ログアウト">
-</form>
- 
+<h1>弁当閲覧・予約</h1>
+    
+<table>
+    <tr><td>弁当名<td>値段
+    <tr><td>弁当画像<td>アレルギー表示<td>注文ボタン
+</table>
+    
 </body>
 </html>
