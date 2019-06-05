0534ba49146dfbeb73be945f3393eea306ef1e4b
takuyayamada <39893033+0105takuyayamada@users.noreply.github.com>
2019-06-05 13:55:16 +0900

    Update sqlUpdate.php

--- htdocs/sqlUpdate.php
+++ htdocs/sqlUpdate.php
@@ -4,27 +4,14 @@
         require_once 'database_conf.php';
         $db = new PDO($dsn, $dbUser, $dbPass);
         //SQL作成・実行
+        
+        //この""の中にSQL文を打つと反映される
+        //ただし"を使ってはいけない
         $sql = "
         
+
+
         
-        
-        CREATE TABLE `bentotable` (
-  `date` date NOT NULL,
-  `name` text NOT NULL,
-  `price` int(11) NOT NULL,
-  `stocks` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- テーブルのデータのダンプ `bentotable`
---
-
-INSERT INTO `bentotable` (`date`, `name`, `price`, `stocks`) VALUES
-('2019-05-30', 'Ａ弁当', 300, 50),
-('2019-05-30', 'Ｂ弁当', 350, 30),
-('2019-05-30', 'スペシャル弁当', 10000, 200);
-
-
 
         ";
 
