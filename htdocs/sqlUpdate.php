e2b13549854ab87192b1341644439ccfa43bfee4
takuyayamada <39893033+0105takuyayamada@users.noreply.github.com>
2019-06-10 21:01:29 +0900

    Update sqlUpdate.php

--- htdocs/sqlUpdate.php
+++ htdocs/sqlUpdate.php
@@ -10,7 +10,22 @@
         $sql = "
 
 
-drop table studentlogintable;
+CREATE TABLE `logintable` (
+  `student` char(7) NOT NULL,
+  `password` varchar(32) DEFAULT NULL,
+  `resumeDate` date DEFAULT NULL,
+  `isVender` boolean
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- テーブルのデータのダンプ `logintable`
+--
+
+INSERT INTO `studentlogintable` (`student`, `password`, `resumeDate`, `isVender`) VALUES
+('1742111', 'murata', NULL, FALSE),
+('1742119', 'yamashita', NULL, FALSE),
+('1742120', 'yamada', NULL, FALSE),
+('0120117', 'shimoda', NULL, TRUE);
 
 
         ";
