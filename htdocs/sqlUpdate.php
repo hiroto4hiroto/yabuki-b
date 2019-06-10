7c993cf6930d27801731a4d3e08286b0ad614d30
takuyayamada <39893033+0105takuyayamada@users.noreply.github.com>
2019-06-10 21:07:19 +0900

    Update sqlUpdate.php

--- htdocs/sqlUpdate.php
+++ htdocs/sqlUpdate.php
@@ -1,24 +1,16 @@
-<?php
-    try {
-        //DBに接続
-        require_once 'database_conf.php';
-        $db = new PDO($dsn, $dbUser, $dbPass);
-        //SQL作成・実行
-        
-        //この""の中にSQL文を打つと反映される
-        //ただし"を使ってはいけない
-        $sql = "
+CREATE TABLE `logintable` (
+  `id` char(7) NOT NULL,
+  `password` varchar(32) DEFAULT NULL,
+  `resumeDate` date DEFAULT NULL,
+  `isVender` boolean
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-drop table logintable;
+--
+-- テーブルのデータのダンプ `logintable`
+--
 
-
-        ";
-
-        $prepare = $db->prepare($sql);
-        $prepare->execute();
-                
-    } catch(PDOException $e) {
-        echo $e->getMessage();
-        die();
-    }
-?>
+INSERT INTO `studentlogintable` (`id`, `password`, `resumeDate`, `isVender`) VALUES
+('1742111', 'murata', NULL, FALSE),
+('1742119', 'yamashita', NULL, FALSE),
+('1742120', 'yamada', NULL, FALSE),
+('0120117', 'shimoda', NULL, TRUE);
