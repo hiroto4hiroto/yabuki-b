1424b9f35f04a8cfdbe6423902bd60967e4368fa
takuyayamada <39893033+0105takuyayamada@users.noreply.github.com>
2019-06-12 13:20:04 +0900

    Create venderMenu.php

---
+++ htdocs/venderMenu.php
@@ -0,0 +1,46 @@
+<?php
+session_start();
+$message = "";
+    //学生でなければ弾く
+if (!isset($_SESSION['USER'])) {
+    header('Location: login.php');
+    exit;
+}
+//ログアウト機能
+if(isset($_POST['logout'])){   
+    $_SESSION = array();
+    session_destroy();
+    header('Location: login.php');
+    exit;
+}
+?>
+ 
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <title>弁当事前予約サービス 業者トップページ</title>
+     <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+ 
+<body style="background-color:#000;">
+<p>弁当事前予約サービス</p>
+<h1>業者トップページ</h1>
+<p>業者番号：<?php echo $_SESSION['USER'] ?>　ログイン中</p>
+
+<?php
+//キャンセル文
+if(!empty($_GET['message']))
+    echo "<p style='color:red;'>". $_GET['message'] ."</p>";
+?>
+
+<input type="button" class="btn-sticky" onclick="location.href='./bentoList.php'" value="弁当閲覧・予約" style="height: calc(var(--fontRatio) * 7.5);"><br>
+<input type="button" class="btn-sticky" onclick="location.href='./check.php'" value="予約確認"><br>
+<input type="button" class="btn-sticky" onclick="location.href='./QRdisplay.php'" value="QRコード表示" style="height: calc(var(--fontRatio) * 7.5);"><br>
+<br>
+<form method="post" action="index.php">
+    <input type="submit" class="btn-sticky" name="logout" value="ログアウト">
+</form>
+ 
+</body>
+</html>
