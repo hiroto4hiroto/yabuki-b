fea095b7059a41382cbbaa34058155f6abcc8e4c
takuyayamada <39893033+0105takuyayamada@users.noreply.github.com>
2019-06-13 14:35:24 +0900

    Create Vdelivery.php

---
+++ htdocs/Vdelivery.php
@@ -0,0 +1,24 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <title>弁当事前予約サービス 引き渡し操作</title>
+     <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+ 
+<body class="vender">
+<p>弁当事前予約サービス</p>
+<h1>引き渡し操作</h1>
+
+<form method="post" action="login.php">
+    <table>
+        <tr><td><label for="user">学生番号</label>
+            <td><input id="user" type="text" name="user">
+    </table>
+    <br>
+    <input class="btn-sticky" type="submit" name="delivery" value="引き渡し">
+</form>
+ 
+</body>
+</html>
