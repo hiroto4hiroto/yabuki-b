f799e877bdb870820ae5e04315c2511d89e5ae3a
takuyayamada <39893033+0105takuyayamada@users.noreply.github.com>
2019-06-13 12:51:48 +0900

    Update sqlUpdate.php

--- htdocs/sqlUpdate.php
+++ htdocs/sqlUpdate.php
@@ -7,7 +7,7 @@
         //ただし"を使ってはいけない
         $sql = "
         
-SELECT * FROM `ordertable` ORDER BY `date`, `price` limit 1;
+SELECT * FROM `ordertable` ORDER BY `date`, `price`;
 
         ";
         $prepare = $db->prepare($sql);
