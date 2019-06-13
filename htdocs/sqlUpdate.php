571d6a1ac64542e234c04176ae71442298dccb63
takuyayamada <39893033+0105takuyayamada@users.noreply.github.com>
2019-06-13 13:01:20 +0900

    Update sqlUpdate.php

--- htdocs/sqlUpdate.php
+++ htdocs/sqlUpdate.php
@@ -7,13 +7,16 @@
         //ただし"を使ってはいけない
         $sql = "
         
-SELECT * FROM `ordertable` ORDER BY `date`;
+INSERT INTO `ordertable` (`check`, `date`, `user`, `name`, `QRid`) VALUES
+(0, '2019-05-20', '1742120', 'い弁当', '254fc572-a766-4522-a013-ff6562026145'),
+(0, '2019-05-20', '1742120', 'い弁当', '254fc572-a766-4522-a013-ff6562026145'),
+(0, '2019-05-21', '1742119', 'ろ弁当', 'fd91c4ee-7817-45df-b0dc-658ff39a9f45');
 
         ";
         $prepare = $db->prepare($sql);
         $prepare->execute();
         $result = $prepare->fetch(PDO::FETCH_ASSOC);
-         var_dump($result);
+        var_dump($result);
                 
     } catch(PDOException $e) {
         echo $e->getMessage();
