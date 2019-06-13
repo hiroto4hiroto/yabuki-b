bf9a27006ab5b9ee5a9da8f4073addf6d6642989
takuyayamada <39893033+0105takuyayamada@users.noreply.github.com>
2019-06-13 12:28:26 +0900

    Update sqlUpdate.php

--- htdocs/sqlUpdate.php
+++ htdocs/sqlUpdate.php
@@ -7,26 +7,7 @@
         //ただし"を使ってはいけない
         $sql = "
         
---
--- テーブルの構造 `ordertable`
---
-
-CREATE TABLE `ordertable` (
-  `check` tinyint(1) DEFAULT 0,
-  `date` date NOT NULL,
-  `user` char(7) NOT NULL,
-  `name` text DEFAULT NULL,
-  `QRid` char(36) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- テーブルのデータのダンプ `ordertable`
---
-
-INSERT INTO `ordertable` (`check`, `date`, `user`, `name`, `QRid`) VALUES
-(0, '2019-05-20', '1742120', 'い弁当', '254fc572-a766-4522-a013-ff6562026145'),
-(0, '2019-05-21', '1742119', 'ろ弁当', 'fd91c4ee-7817-45df-b0dc-658ff39a9f45');
-
+SELECT * FROM ordertable ORDER BY date, price ASC;
 
         ";
         $prepare = $db->prepare($sql);
