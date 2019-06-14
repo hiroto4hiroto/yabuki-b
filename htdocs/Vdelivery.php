24eab9c9ce820e16b5581d02b47e938ee09bd500
yuyayamashita02 <49549012+yuyayamashita02@users.noreply.github.com>
2019-06-14 13:21:42 +0900

    Update Vdelivery.php

--- htdocs/Vdelivery.php
+++ htdocs/Vdelivery.php
@@ -1,5 +1,42 @@
 <?php
-    
+session_start();
+$list = "";
+    //業者でなければ弾く
+if (!isset($_SESSION['VENDER'])) {
+    header('Location: login.php');
+    exit;
+}
+    try {
+        require_once 'database_conf.php';
+        $db = new PDO($dsn, $dbUser, $dbPass);
+        
+        //予約一覧作成
+        //SQL作成・実行
+        $sql = 'SELECT * FROM ordertable ORDER BY date;';
+        $prepare = $db->prepare($sql);
+        $prepare->execute();
+        
+        $list .= '予約一覧';
+        $list .= '<br><table style="width: 80vw; height: 2em;"><tr>';
+        $list .= '<td style="width: 5vw;">受取';
+        $list .= '<td style="width: 10vw;">日付';
+        $list .= '<td style="width: 10vw;">学生番号';
+        $list .= '<td style="width: 20vw;">弁当名';
+        $list .= '<td style="width: 35vw;">UUID';
+        foreach ($prepare->fetchAll(PDO::FETCH_ASSOC) as $result)
+        {
+            $list .= '<tr>';
+            $list .= '<td class="orderText">'. $result["check"];
+            $list .= '<td class="orderText">'. $result["date"];
+            $list .= '<td class="orderText">'. $result["user"];
+            $list .= '<td class="orderText">'. $result["name"];
+            $list .= '<td class="orderText">'. $result["QRid"];
+        }
+        $list .= '</table>';
+    } catch(PDOException $e) {
+        echo $e->getMessage();
+        die();
+    }
 ?>
 <!DOCTYPE html>
 <html>
