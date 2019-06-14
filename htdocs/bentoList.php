42b2c7215ab4310bf4c7426a090e4404b2deeace
takuyayamada <39893033+0105takuyayamada@users.noreply.github.com>
2019-06-14 19:54:59 +0900

    Update bentoList.php

--- htdocs/bentoList.php
+++ htdocs/bentoList.php
@@ -30,7 +30,7 @@ $isDebug = true;
 
             //注文リストに一件追加
             //$sql = "INSERT INTO `ordertable` (`check`, `date`, `user`, `name`, `QRid`) VALUES (0, '". $getdate ."' + 1, :user, :name, :QRid)";
-            $sql = "INSERT INTO `ordertable` (`check`, `date`, `user`, `name`, `QRid`) VALUES (0, '2019-07-19' + 1, :user, :name, :QRid)";
+            $sql = "INSERT INTO `ordertable` (`check`, `date`, `user`, `name`, `QRid`) VALUES (0, date() + 1, :user, :name, :QRid)";
             $result = $db->prepare($sql);
             $params = array(':user' => $_SESSION['USER'], ':name' => $_GET['order'], ':QRid' => $UUID);
             $result->execute($params);
