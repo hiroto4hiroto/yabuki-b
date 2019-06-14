71fe9c79c461496affe072236e3f29facb3d4a1f
takuyayamada <39893033+0105takuyayamada@users.noreply.github.com>
2019-06-14 21:48:50 +0900

    Update bentoList.php

--- htdocs/bentoList.php
+++ htdocs/bentoList.php
@@ -32,7 +32,8 @@ $isDebug = true;
             $sql = "INSERT INTO `ordertable` (`check`, `date`, `user`, `name`, `QRid`) VALUES (0, '". $getdate ."' + INTERVAL 1 DAY, :user, :name, :QRid)";
             //$sql = "INSERT INTO `ordertable` (`check`, `date`, `user`, `name`, `QRid`) VALUES (0, '". $getdate ."' + INTERVAL 1 DAY, '1742120', 'とてもいい弁当', '0120-117-117');";
             $result = $db->prepare($sql);
-            $params = array(':user' => $_SESSION['USER'], ':name' => $_GET['order'], ':QRid' => $UUID);
+            $params = array(':user' => 1742120, ':name' => ほげ, ':QRid' => $UUID);
+            //$params = array(':user' => $_SESSION['USER'], ':name' => $_GET['order'], ':QRid' => $UUID);
             $result->execute($params);
         
             //トップページに移動
