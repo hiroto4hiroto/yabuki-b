4923cb6da4284b32bb3178ef5d66f579c4999a45
takuyayamada <39893033+0105takuyayamada@users.noreply.github.com>
2019-06-14 19:57:27 +0900

    Update bentoList.php

--- htdocs/bentoList.php
+++ htdocs/bentoList.php
@@ -30,10 +30,10 @@ $isDebug = true;
 
             //注文リストに一件追加
             //$sql = "INSERT INTO `ordertable` (`check`, `date`, `user`, `name`, `QRid`) VALUES (0, '". $getdate ."' + 1, :user, :name, :QRid)";
-            $sql = "INSERT INTO `ordertable` (`check`, `date`, `user`, `name`, `QRid`) VALUES (0, date() + 1, :user, :name, :QRid)";
+            $sql = "INSERT INTO `ordertable` (`check`, `date`, `user`, `name`, `QRid`) VALUES (0, '2019-07-19', '1742120', 'とてもいい弁当', '0120-117-117');";
             $result = $db->prepare($sql);
-            $params = array(':user' => $_SESSION['USER'], ':name' => $_GET['order'], ':QRid' => $UUID);
-            $result->execute($params);
+            //$params = array(':user' => $_SESSION['USER'], ':name' => $_GET['order'], ':QRid' => $UUID);
+            $result->execute(); //$params);
         
             //トップページに移動
             header('Location: index.php?message='. $_GET['order'] .'を予約しました。');
