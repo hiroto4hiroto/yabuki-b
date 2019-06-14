91e45d7b65fa40942f437190a6e6f508cfaa9d3e
yuyayamashita02 <49549012+yuyayamashita02@users.noreply.github.com>
2019-06-14 16:30:28 +0900

    Update Vdelivery.php

--- htdocs/Vdelivery.php
+++ htdocs/Vdelivery.php
@@ -33,7 +33,7 @@ if (!isset($_SESSION['VENDER'])) {
         //$list .= '<td style="width: 35vw;">UUID';
         foreach ($prepare->fetchAll(PDO::FETCH_ASSOC) as $result)
         {
-            if ($result["date"] == date())
+            if ($result["date"] == date("Y-M-D"))
             {
                 $list .= '<tr>';
                 $list .= '<td class="orderText">'. $result["check"];
