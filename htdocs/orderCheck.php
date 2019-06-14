2b54cd6a42e42892aa6b1b31db11fef45e874cec
yuyayamashita02 <49549012+yuyayamashita02@users.noreply.github.com>
2019-06-14 13:48:04 +0900

    Create orderCheck.php

---
+++ htdocs/orderCheck.php
@@ -0,0 +1,88 @@
+<?php
+session_start();
+$list = "";
+    //学生でなければ弾く
+if (!isset($_SESSION['USER'])) {
+    header('Location: login.php');
+    exit;
+}
+    try {
+        require_once 'database_conf.php';
+        $db = new PDO($dsn, $dbUser, $dbPass);
+        
+        if( !empty($_POST['delivery']) ){
+            //予約一覧作成
+            //SQL作成・実行
+            $sql = 'UPDATE ordertable SET `check` = 1 WHERE user = '. $_POST['user'];
+            $prepare = $db->prepare($sql);
+            $prepare->execute();
+        }
+        
+        //予約一覧作成
+        //SQL作成・実行
+        $sql = 'SELECT * FROM ordertable ORDER BY date;';
+        $prepare = $db->prepare($sql);
+        $prepare->execute();
+        
+        $list .= '予約一覧';
+        $list .= '<br><table style="width: 80vw; height: 2em;"><tr>';
+        $list .= '<td style="width: 5vw;">受取';
+        $list .= '<td style="width: 10vw;">日付';
+        $list .= '<td style="width: 10vw;">学生番号';
+        $list .= '<td style="width: 20vw;">弁当名';
+        $list .= '<td style="width: 35vw;">UUID';
+        foreach ($prepare->fetchAll(PDO::FETCH_ASSOC) as $result)
+        {
+            $list .= '<tr>';
+            $list .= '<td class="orderText">'. $result["check"];
+            $list .= '<td class="orderText">'. $result["date"];
+            $list .= '<td class="orderText">'. $result["user"];
+            $list .= '<td class="orderText">'. $result["name"];
+            $list .= '<td class="orderText">'. $result["QRid"];
+        }
+        $list .= '</table>';
+    } catch(PDOException $e) {
+        echo $e->getMessage();
+        die();
+    }
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <title>弁当事前予約サービス 予約確認・キャンセル</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <style type="text/css">
+    <!--
+    tr td {border-style: solid;}
+    -->
+    </style>
+</head>
+ 
+<script language="javascript" type="text/javascript">
+    function OnButtonClick(name) {
+        var res = confirm('「' + name + '」を予約しますか？');
+        if(res) {
+            //予約可能時間前か
+            if (new Date().getHours() < 15){
+                window.location.href =　location.href + '?order=' + name;
+            }else{
+                alert('予約可能時間を過ぎたため予約できませんでした。');
+                window.location.href =　location.href + '?order=' + name;
+            }
+        }
+        else {
+            alert('予約はされませんでした。');
+        }
+    }
+</script>
+    
+<body>
+<p>弁当事前予約サービス</p>
+<h1>予約確認・キャンセル</h1>
+<p>あなたの予約した弁当はこちらになります。</p>
+<?php echo $list; ?>
+<hr>
+<p>キャンセル可能時間は前日の15:00までとなります．</p>
+</body>
+</html>
