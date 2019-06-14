9bc4ab594a0193f832411493319f8771cccadd7a
yuyayamashita02 <49549012+yuyayamashita02@users.noreply.github.com>
2019-06-14 16:25:57 +0900

    Update Vdelivery.php

--- htdocs/Vdelivery.php
+++ htdocs/Vdelivery.php
@@ -36,19 +36,19 @@ if (!isset($_SESSION['VENDER'])) {
             if ($result["date"] == '2019-07-19')
             {
                 $list .= '<tr>';
+                $list .= '<td class="orderText">'. $result["check"];
+                $list .= '<td class="orderText">'. $result["date"];
+                $list .= '<td class="orderText">'. $result["user"];
+                $list .= '<td class="orderText">'. $result["name"];
+                //$list .= '<td class="orderText">'. $result["QRid"];
+            }
+            else {
+                $list .= '<tr>';
                 $list .= '<td>'. $result["check"];
                 $list .= '<td>'. $result["date"];
                 $list .= '<td>'. $result["user"];
                 $list .= '<td>'. $result["name"];
-            //$list .= '<td class="orderText">'. $result["QRid"];
-            }
-            else {
-            $list .= '<tr>';
-            $list .= '<td class="orderText">'. $result["check"];
-            $list .= '<td class="orderText">'. $result["date"];
-            $list .= '<td class="orderText">'. $result["user"];
-            $list .= '<td class="orderText">'. $result["name"];
-            //$list .= '<td class="orderText">'. $result["QRid"];
+                //$list .= '<td class="orderText">'. $result["QRid"];
             }
         }
         $list .= '</table>';
