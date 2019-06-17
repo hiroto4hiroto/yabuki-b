bbd7fcae3e5b6174ae7a4471c8f2190c62028e6c
takuyayamada <39893033+0105takuyayamada@users.noreply.github.com>
2019-06-17 15:26:59 +0900

    Update sqlUpdate.php

--- htdocs/sqlUpdate.php
+++ htdocs/sqlUpdate.php
@@ -23,7 +23,7 @@ INSERT INTO `ordertable` (`check`, `date`, `user`, `name`, `QRid`) VALUES
         $prepare = $db->prepare($sql);
         $prepare->execute();
         $result = $prepare->fetch(PDO::FETCH_ASSOC);
-        echo $result;
+        print_r $result;
                 
     } catch(PDOException $e) {
         echo $e->getMessage();
