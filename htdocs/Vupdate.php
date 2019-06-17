fb56126bc5fcbacb84b6dc3c72762cc5075c5225
takuyayamada <39893033+0105takuyayamada@users.noreply.github.com>
2019-06-17 16:58:55 +0900

    Update Vupdate.php

--- htdocs/Vupdate.php
+++ htdocs/Vupdate.php
@@ -10,50 +10,33 @@ if (!isset($_SESSION['VENDER'])) {
         require_once 'database_conf.php';
         $db = new PDO($dsn, $dbUser, $dbPass);
         
-        if( !empty($_POST['delivery']) ){
-            //予約一覧作成
-            //SQL作成・実行
-            $sql = 'UPDATE ordertable SET `check` = 1 WHERE user = '. $_POST['user'];
-            $prepare = $db->prepare($sql);
-            $prepare->execute();
-        }
-        
         //予約一覧作成
         //SQL作成・実行
         $sql = 'SELECT * FROM bentotable ORDER BY `view`, `date`, `price`, `name`;';
         $prepare = $db->prepare($sql);
         $prepare->execute();
         
-        $list .= '予約一覧';
+        $list .= '登録弁当一覧';
         $list .= '<br><table style="width: 80vw; height: 2em;"><tr>';
-        $list .= '<td style="width: 5vw;">受取';
-        $list .= '<td style="width: 10vw;">日付';
-        $list .= '<td style="width: 10vw;">学生番号';
+        $list .= '<td style="width: 5vw;">販売表示';
+        $list .= '<td style="width: 10vw;">販売日';
         $list .= '<td style="width: 20vw;">弁当名';
-        //$list .= '<td style="width: 35vw;">UUID';
+        $list .= '<td style="width: 10vw;">価格';
+        $list .= '<td style="width: 10vw;">在庫';
+        $list .= '<td style="width: 35vw;">画像';
         foreach ($prepare->fetchAll(PDO::FETCH_ASSOC) as $result)
         {
+            $plusClass = ' class="todayOrder" ';
             if ($result["date"] == $getdate)
-            {
-                $list .= '<tr>';
-            if ($result["check"] == 1)
-                $list .= '<td class="todayOrder" style="color:blue;">完了';
-            else $list .= '<td class="todayOrder" style="color:red;">未了';
-                $list .= '<td class="todayOrder">'. $result["date"];
-                $list .= '<td class="todayOrder">'. $result["user"];
-                $list .= '<td class="todayOrder">'. $result["name"];
-                //$list .= '<td class="todayOrder">'. $result["QRid"];
-            }
-            else {
-                $list .= '<tr>';
-                if ($result["check"] == 1)
-                    $list .= '<td style="color:blue;">完了';
-                else $list .= '<td style="color:red;">未了';
-                $list .= '<td>'. $result["date"];
-                $list .= '<td>'. $result["user"];
-                $list .= '<td>'. $result["name"];
-                //$list .= '<td class="orderText">'. $result["QRid"];
-            }
+            
+            $list .= '<tr>';
+            if ($result["view"] == 1)
+                $list .= '<td'. $plusClass .'style="color:blue;">公開';
+            else $list .= '<td'. $plusClass .' style="color:red;">未公開';
+            $list .= '<td'. $plusClass .'>'. $result["date"];
+            $list .= '<td'. $plusClass .'>'. $result["user"];
+            $list .= '<td'. $plusClass .'>'. $result["name"];
+            $list .= '<td'. $plusClass .' class="orderText">'. $result["QRid"];
         }
         $list .= '</table>';
     } catch(PDOException $e) {
@@ -74,7 +57,7 @@ if (!isset($_SESSION['VENDER'])) {
 <br>
 <form method="post" action="Vupdate.php" enctype="multipart/form-data">
     <table>
-        <tr><td><label for="view">弁当名</label>
+        <label for="name"><tr><td><弁当名</label>
             <td><input id="name" type="text" name="name">
         <tr><td><label for="view">販売表示</label>
             <td><input id="view" type="checkbox" name="view">
