b9f7d918869a50a05cf46ef2ffae0daaabca5611
takuyayamada <39893033+0105takuyayamada@users.noreply.github.com>
2019-06-17 17:03:54 +0900

    Update Vdelivery.php

--- htdocs/Vdelivery.php
+++ htdocs/Vdelivery.php
@@ -41,7 +41,7 @@ if (!isset($_SESSION['VENDER'])) {
             if ($result["check"] == 1)
                 $list .= '<td'. $plusClass .' style="color:blue;">完了';
             else $list .= '<td'. $plusClass .' style="color:red;">未了';
-            $list .= '<td'. $plusClass .'>'. $result["date"];
+            $list .= '<td class="todayOrder" >'. $result["date"];
             $list .= '<td'. $plusClass .'>'. $result["user"];
             $list .= '<td'. $plusClass .'>'. $result["name"];
         }
