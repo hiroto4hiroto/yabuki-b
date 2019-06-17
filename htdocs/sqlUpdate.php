dcc8d725c11ba54d15cb0e10bc24e81849f121be
takuyayamada <39893033+0105takuyayamada@users.noreply.github.com>
2019-06-17 15:26:31 +0900

    Update sqlUpdate.php

--- htdocs/sqlUpdate.php
+++ htdocs/sqlUpdate.php
@@ -16,14 +16,14 @@ INSERT INTO `ordertable` (`check`, `date`, `user`, `name`, `QRid`) VALUES
 (0, '2019-07-20', '1742120', 'い弁当', '254fc572-a766-4522-a013-ff6562026145'),
 (0, '2019-07-21', '1742120', 'は弁当', '354fc572-a766-4522-a013-ff6562026145'),
 (0, '2019-07-22', '1742120', 'に弁当', '454fc572-a766-4522-a013-ff6562026145'),
-(0, '2019-07-23', '1742119', 'ほ弁当', '5d91c4ee-7817-45df-b0dc-658ff39a9f45');
+(0, '2019-07-23', '1742119', 'ほ弁当', '5d91c4ee-7817-45df-b0dc-658ff39a9f4);
 */
 
         ";
         $prepare = $db->prepare($sql);
         $prepare->execute();
         $result = $prepare->fetch(PDO::FETCH_ASSOC);
-        var_dump($result);
+        echo $result;
                 
     } catch(PDOException $e) {
         echo $e->getMessage();
