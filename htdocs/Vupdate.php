03d8348bfa5de4806286216c93a691e4d3559e30
takuyayamada <39893033+0105takuyayamada@users.noreply.github.com>
2019-06-17 17:21:22 +0900

    Update Vupdate.php

--- htdocs/Vupdate.php
+++ htdocs/Vupdate.php
@@ -70,7 +70,7 @@ if (!isset($_SESSION['VENDER'])) {
             <td><input id="stocks" type="number" name="stocks">
         <tr><td><label for="image">画像</label>
             <td><input id="image" type="file" accept="image/*.jpg">
-        <tr><td colspan="2"><input class="btn-sticky" type="submit" name="update" value="登録・更新">
+        <tr><td colspan="2"><input class="btn-sticky" type="submit" name="update" value="登録・更新" style"width:100%; height:100%">
     </table>
 </form>
 <br>
