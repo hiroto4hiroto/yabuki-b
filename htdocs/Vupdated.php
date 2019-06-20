85fedd522b43d34d3a97fb4c32d3f1cc51a4560f
takuyayamada <39893033+0105takuyayamada@users.noreply.github.com>
2019-06-20 23:07:23 +0900

    Update Vupdated.php

--- htdocs/Vupdated.php
+++ htdocs/Vupdated.php
@@ -14,6 +14,13 @@ if (!isset($_SESSION['VENDER'])) {
     <meta charset="utf-8"/>
     <title>弁当事前予約サービス 更新完了</title>
      <link rel="stylesheet" type="text/css" href="style.css">
+    <script>
+function enter(){
+    if( window.event.keyCode == 13 ){
+        window.location.href = location.href='./Vupdate.php';
+    }
+}
+</script>
 </head>
  
 <body style="background-color:#ffb;">
@@ -25,6 +32,9 @@ if(!empty($_GET['message']))
     echo "<p style='color:red;'>". $_GET['message'] ."</p>";
 ?>
 <p>※記述されない場合は更新されていません</p>
-<input type="submit" class="btn-sticky" onclick="location.href='./Vupdate.php'" value="戻る"><br>
+<form name="form1">
+  <input type="" >
+</form>
+<input type="submit" class="btn-sticky" onkeypress="enter();" value="戻る"><br>
 </body>
 </html>
