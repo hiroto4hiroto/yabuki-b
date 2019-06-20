6b7440c6056fa11f269a2b1c6a7da5e470928b21
takuyayamada <39893033+0105takuyayamada@users.noreply.github.com>
2019-06-21 00:49:45 +0900

    Update Vindex.php

--- htdocs/Vindex.php
+++ htdocs/Vindex.php
@@ -34,7 +34,7 @@ if(!empty($_GET['message']))
     echo "<p style='color:red;'>". $_GET['message'] ."</p>";
 ?>
 <input type="button" class="btn-sticky" onclick="location.href='./Vupdate.php'" value="弁当情報の登録・更新" style="height: calc(var(--fontRatio) * 7.5);"><br>
-<input type="button" class="btn-sticky" onclick="location.href='./bentoList.php'" value="実際の弁当一覧表示を見る"><br>
+<input type="button" class="btn-sticky" onclick="location.href='./bentoList.php'" value="実際の弁当一覧を見る"><br>
 <input type="button" class="btn-sticky" onclick="location.href='./VorderCheck.php'" value="予約数の確認" style="height: calc(var(--fontRatio) * 7.5);"><br>
 <input type="button" class="btn-sticky" onclick="location.href='./Vdelivery.php'" value="引き渡し操作" style="height: calc(var(--fontRatio) * 7.5);"><br>
 <br>
