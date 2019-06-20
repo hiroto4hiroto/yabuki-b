9088fc1adef2df6b761ed78850ae35b189d79ca5
takuyayamada <39893033+0105takuyayamada@users.noreply.github.com>
2019-06-20 12:30:04 +0900

    Update sqlUpdate.php

--- htdocs/sqlUpdate.php
+++ htdocs/sqlUpdate.php
@@ -6,7 +6,8 @@
         //この""の中にSQL文を打つと反映される
         //ただし"を使ってはいけない
         $sql = "
-
+        
+        UPDATE INTO `imagetable` (`id`, `image`) VALUES ( 5, 89768769) WHERE `id` = 5;
         select * from `imagetable` where id = 5;
 
         ";
