48b4572c03861b8a9a2996a36d122b1a2d2e0897
takuyayamada <39893033+0105takuyayamada@users.noreply.github.com>
2019-06-20 22:49:50 +0900

    Update Vupdated.php

--- htdocs/Vupdated.php
+++ htdocs/Vupdated.php
@@ -1,1 +1,30 @@
+<?php
+session_start();
+$message = "";
+    //業者でなければ弾く
+if (!isset($_SESSION['USER'])) {
+    header('Location: login.php');
+    exit;
+}
+?>
+ 
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <title>弁当事前予約サービス 更新完了</title>
+     <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+ 
+<body style="background-color:#ffb;">
+<p>弁当事前予約サービス</p>
+<h1>更新完了</h1>
 
+<?php
+if(!empty($_GET['message']))
+    echo "<p style='color:red;'>". $_GET['message'] ."</p>";
+?>
+
+<input type="button" class="btn-sticky" onclick="location.href='./Vupdate.php'" value="戻る"><br>
+</body>
+</html>
