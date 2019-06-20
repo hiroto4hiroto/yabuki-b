4477d1afcc15cfa7add29a72b661d2aa27d754af
takuyayamada <39893033+0105takuyayamada@users.noreply.github.com>
2019-06-20 20:42:53 +0900

    Update sqlUpdate.php

--- htdocs/sqlUpdate.php
+++ htdocs/sqlUpdate.php
@@ -25,5 +25,5 @@ header('Content-type: image/jpeg');
     }
 ?>
 <html>
-<div style="background-image:url('<?php echo $result['image'] ?>'); width: 50vw; height: 50vh;"></div>
+<div style="width: 50vw; height: 50vh; background-image:url(<?php echo $result['image'] ?>);"></div>
 </html>
