5ca022fe76c08332bf5859e9f5e725346bddc75e
takuyayamada <39893033+0105takuyayamada@users.noreply.github.com>
2019-06-21 14:07:50 +0900

    Update sqlUpdate.php

--- htdocs/sqlUpdate.php
+++ htdocs/sqlUpdate.php
@@ -7,8 +7,8 @@
         //ただし"を使ってはいけない
         $sql = "
         
-       
-       SELECT * FROM bentotable;
+       INSERT INTO `ordertable` VALUES (`check`, `user`, `id`, `QRid`) VALUES (0, '1742120', 1, 'hogeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee');
+       SELECT * FROM ordertable;
         
         
         
