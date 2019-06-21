17805e3ec783e7160dab96568e3af13d1f73101d
takuyayamada <39893033+0105takuyayamada@users.noreply.github.com>
2019-06-21 14:31:33 +0900

    Update orderCheck.php

--- htdocs/orderCheck.php
+++ htdocs/orderCheck.php
@@ -14,7 +14,7 @@ if (!isset($_SESSION['USER'])) {
         //SQL作成・実行
         //$sql = "SELECT ordertable.check as `check`, ordertable.date as `date`, ordertable.name as `name`, bentotable.price as `price`";
         //$sql .= " FROM ordertable INNER JOIN bentotable ON ordertable.name = bentotable.name AND user = '". $_SESSION['USER'] ."'";
-        $sql = "SELECT order.check, bento.date, bento.name, bento.price FROM ordertable as order LEFT JOIN bentotable as bento ON order.id = bento.id;";
+        $sql = "SELECT order.check, bento.date, bento.name, bento.price FROM ordertable as order LEFT OUTER JOIN bentotable as bento ON order.id = bento.id;";
         $prepare = $db->prepare($sql);
         $prepare->execute();
         
