cddb38681467bfef1d0bbca57d689d6b4198ac3d
takuyayamada <39893033+0105takuyayamada@users.noreply.github.com>
2019-06-21 13:56:39 +0900

    Update sqlUpdate.php

--- htdocs/sqlUpdate.php
+++ htdocs/sqlUpdate.php
@@ -7,15 +7,9 @@
         //ただし"を使ってはいけない
         $sql = "
         
-        
-drop table ordertable;
-
-CREATE TABLE `ordertable` (
-  `check` tinyint(1) DEFAULT 0,
-  `user` char(7) NOT NULL,
-  `id` int(11) NOT NULL,
-  `QRid` char(36) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+       
+INSERT INTO `bentotable` (`check`,`user`,`id`,`QRid`) VALUES
+(0, '1742120', 1, 'kokonihaQRidgahairuyo');
         
         ";
         $prepare = $db->prepare($sql);
