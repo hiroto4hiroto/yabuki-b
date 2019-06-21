c5af072faa6e23fde589582c9975a6b3b125053b
takuyayamada <39893033+0105takuyayamada@users.noreply.github.com>
2019-06-21 14:15:21 +0900

    Update sqlUpdate.php

--- htdocs/sqlUpdate.php
+++ htdocs/sqlUpdate.php
@@ -7,16 +7,7 @@
         //ただし"を使ってはいけない
         $sql = "
         
-drop table ordertable;
-
-CREATE TABLE ordertable (
-  `check` tinyint(1) DEFAULT 0,
-  `user` char(7) NOT NULL,
-  `id` int(11) NOT NULL,
-  `QRid` char(36) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-INSERT INTO `ordertable` VALUES (`check`, `user`, `id`, `QRid`) VALUES (0, '1742120', 1, 'hogeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee');
+select * from ordertable;
         
         ";
         $prepare = $db->prepare($sql);
