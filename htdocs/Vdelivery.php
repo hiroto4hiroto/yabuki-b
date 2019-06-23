84b9f084109d7cbf8e2311807a990240a8db922f
takuyayamada <39893033+0105takuyayamada@users.noreply.github.com>
2019-06-23 12:13:10 +0900

    Update Vdelivery.php

--- htdocs/Vdelivery.php
+++ htdocs/Vdelivery.php
@@ -20,9 +20,9 @@ if (!isset($_SESSION['VENDER'])) {
         
         //予約一覧作成
         //SQL作成・実行
-        $sql = 'SELECT `ordertable`.check, `bentotable`.date, `ordertable`.user, `ordertable`.id, `bentotable`.name ';
-        $sql = 'FROM `ordertable` as `order` RIGHT JOIN `bentotable` as `bento` ON `ordertable`.id = `bentotable`.id ';
-        $sql = 'ORDER BY `ordertable`.check, `bentotable`.date, `ordertable`.id, `bentotable`.name;';
+        $sql = 'SELECT `order`.check as `check`, `bento`.date as `date`, `order`.user as `user`, `order`.id as `id`, `bento`.name as `name` ';
+        $sql = 'FROM `order` as `order` RIGHT JOIN `bento` as `bento` ON `order`.id = `bento`.id ';
+        $sql = 'ORDER BY `order`.check, `bento`.date, `order`.id, `bento`.name;';
         $prepare = $db->prepare($sql);
         $prepare->execute();
         
@@ -30,7 +30,8 @@ if (!isset($_SESSION['VENDER'])) {
         $list .= '<br><table style="width: 80vw; height: 2em;"><tr>';
         $list .= '<td style="width: 5vw;">受取';
         $list .= '<td style="width: 10vw;">日付';
-        $list .= '<td style="width: 10vw;">学生番号';
+        $list .= '<td style="width: 10vw;">学生番号'; 
+        $list .= '<td style="width: 10vw;">弁当ID';
         $list .= '<td style="width: 20vw;">弁当名';
         //$list .= '<td style="width: 35vw;">UUID';
         foreach ($prepare->fetchAll(PDO::FETCH_ASSOC) as $result)
@@ -44,6 +45,7 @@ if (!isset($_SESSION['VENDER'])) {
             else $list .= '<td'. $plusClass .' style="color:red;">未了';
             $list .= '<td'. $plusClass .'>'. $result["date"];
             $list .= '<td'. $plusClass .'>'. $result["user"];
+            $list .= '<td'. $plusClass .'>'. $result["id"];
             $list .= '<td'. $plusClass .'>'. $result["name"];
         }
         $list .= '</table>';
