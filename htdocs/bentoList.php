c064690c4be3a0c244264d698ea93ac67288d45a
takuyayamada <39893033+0105takuyayamada@users.noreply.github.com>
2019-06-24 14:11:36 +0900

    Update bentoList.php

--- htdocs/bentoList.php
+++ htdocs/bentoList.php
@@ -78,8 +78,10 @@ if (isset($_SESSION['VENDER'])) $isVENDER = 'true';
             $list .= '<td style="max-width: 30%;">'. $result["price"] .'円';
             $list .= '<tr style="width: 100%; max-height: 100%;">';
             $list .= '<td style="min-width: 70%; background-image: url(\'data:image/jpeg;base64,'. base64_encode($result["image"]) .'\'); background-size: cover; background-position: center;">';
+            
             //日付と時間帯によって, 数量によって押せなくする
-            if ($isDebug || (date("G") < 15 && (string)date("Y-m-d", strtotime("+1 day")) == $result["date"] && $result["stocks"] > 0) ){
+            //if ($isDebug || (date("G") < 15 && (string)date("Y-m-d", strtotime( $getdate )) == $result["date"] && $result["stocks"] > 0) ){
+            if ($isDebug || (date("G") < 15 && new DateTime( $getdate )->modify('+1 day')->format('Y-M-D') == $result["date"] && $result["stocks"] > 0) ){
                 $list .= '<td style="max-width: 30%;">';
                 $list .= '<input type="button" class="btn-sticky" onclick="OnButtonClick(\''.$result["id"].'\');" ';
                 $list .= 'value="予約する" style="width: 100%; height: 100%"></input>';
