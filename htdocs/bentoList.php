7f5a8eb3024025770721466097ae372e7f9ae47f
takuyayamada <39893033+0105takuyayamada@users.noreply.github.com>
2019-06-26 10:53:03 +0900

    Update bentoList.php

--- htdocs/bentoList.php
+++ htdocs/bentoList.php
@@ -66,9 +66,6 @@ if (isset($_SESSION['VENDER'])) $isVENDER = 'true';
         $prepare = $db->prepare($sql);
         $list = "";
         $prepare->execute();
-        //$hoge = date_format(date_modify(DateTime::createFromFormat('Y-M-D', $getdate), '+1 day'), 'Y-M-D');
-        $hoge = date( "Y-m-d", strtotime( "2019-07-19 +1 day" ) );
-        echo $hoge;
 
         foreach ($prepare->fetchAll(PDO::FETCH_ASSOC) as $result)
         {
@@ -84,7 +81,7 @@ if (isset($_SESSION['VENDER'])) $isVENDER = 'true';
             
             //15時前で前日で在庫があれば押せる
             //if ($isDebug || (date("G") < 15 && (string)date("Y-m-d", strtotime( $getdate )) == $result["date"] && $result["stocks"] > 0) ){
-            if ($isDebug || (date("G") < 15 && $hoge == $result["date"] && $result["stocks"] > 0) )
+            if ($isDebug || (date("G") < 15 && $result["date"] == date( "Y-m-d", strtotime("2019-07-19 +1 day") ) && $result["stocks"] > 0) )
             {
                 $list .= '<td style="max-width: 30%;">';
                 $list .= '<input type="button" class="btn-sticky" onclick="OnButtonClick(\''.$result["id"].'\');" ';
