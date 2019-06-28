c083437d2e41674f527d37e06209a6c810b8e7ad
takuyayamada <39893033+0105takuyayamada@users.noreply.github.com>
2019-06-28 14:55:50 +0900

    Update database_conf.php

--- htdocs/database_conf.php
+++ htdocs/database_conf.php
@@ -1,6 +1,6 @@
 <?php
 $debug = 0;
-$getdate = '2019-06-29'; //date("Y-m-d"); //'2019-07-19'
+$getdate = date("Y-m-d"); //'2019-07-19'
 // データベース設定（サーバで公開するとき）
 $dbServer = '127.0.0.1';
 $dbUser = $_SERVER['MYSQL_USER'];
