db56fa733b236d87c039e58662919b61e1ceb1fd
takuyayamada <39893033+0105takuyayamada@users.noreply.github.com>
2019-07-03 15:39:03 +0900

    Update database_conf.php

--- htdocs/database_conf.php
+++ htdocs/database_conf.php
@@ -1,6 +1,7 @@
 <?php
 $debug = 0;
 $getdate = '2019-07-19';//date("Y-m-d"); //'2019-07-19'
+$getclosetime = 15;
 // データベース設定（サーバで公開するとき）
 $dbServer = '127.0.0.1';
 $dbUser = $_SERVER['MYSQL_USER'];
