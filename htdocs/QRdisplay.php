0e9a972a26c3e6e3cdaa8bac85fd4ca3dab3fe4d
takuyayamada <39893033+0105takuyayamada@users.noreply.github.com>
2019-07-05 16:59:21 +0900

    Update QRdisplay.php

--- htdocs/QRdisplay.php
+++ htdocs/QRdisplay.php
@@ -67,8 +67,10 @@ if (!isset($_SESSION['USER'])) {
 	
 	
 <script type="text/javascript">
+	var uuid = <?php echo $uuid;?>;
+	if (<?php echo $uuid;?> != '') {
 	      	document.getElementById("QRview").innerHTML = 
-			'<img src="https://chart.apis.google.com/chart?chs=512x512&cht=qr&chl=<?php echo $uuid;?>" width="80%">';
-	if (<?php echo $uuid;?> == '') document.getElementById("QRview").innerHTML = '';
+			'<img src="https://chart.apis.google.com/chart?chs=512x512&cht=qr&chl=' + uuid + '" width="80%">';
+	}
 </script>
 </html>
