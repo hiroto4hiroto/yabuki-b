ae8d1864d67fbebda22b0c69ca08926f5415ebae
takuyayamada <39893033+0105takuyayamada@users.noreply.github.com>
2019-07-05 15:41:18 +0900

    Update QRdisplay.php

--- htdocs/QRdisplay.php
+++ htdocs/QRdisplay.php
@@ -42,7 +42,7 @@ if (!isset($_SESSION['USER'])) {
 
 </body>
 <script type="text/javascript">
-	var qrHeight = String(Math.round(window.innerWidth * 0.8));
+	var qrHeight = String(Math.round(window.innerWidth));
 	document.getElementById("QRview").innerHTML =
 		'<img src="https://chart.apis.google.com/chart?chs=' + 
 		qrHeight + 'x' + qrHeight +
