d9191a5f4646681d7b90529e3133d470fbf80086
takuyayamada <39893033+0105takuyayamada@users.noreply.github.com>
2019-07-05 13:55:29 +0900

    Update database_conf.php

--- htdocs/database_conf.php
+++ htdocs/database_conf.php
@@ -4,7 +4,7 @@ $debug = 0;
 //日にちの設定　date("Y-m-d");　とすればリアルタイムになる
 $getdate = '2019-07-19';//date("Y-m-d"); //'2019-07-19'
 //予約・キャンセルの締切時間
-$getclosetime = 15;
+$getclosetime = 12;
 // データベース設定（サーバで公開するとき）
 $dbServer = '127.0.0.1';
 $dbUser = $_SERVER['MYSQL_USER'];
