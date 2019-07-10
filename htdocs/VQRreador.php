648001afbaf30b41968bfcdf29e2a57b91e1ab0b
takuyayamada <39893033+0105takuyayamada@users.noreply.github.com>
2019-07-11 01:31:39 +0900

    Update VQRreador.php

--- htdocs/VQRreador.php
+++ htdocs/VQRreador.php
@@ -45,10 +45,9 @@ if (!isset($_SESSION['VENDER'])) {
             $sql .= " WHERE `order`.QRid = '". $QRid ."'";
             $sql .= " ORDER BY `order`.check, `bento`.date, `order`.user, `order`.id;";
             $prepare = $db->prepare($sql);
-            $emptyCheck = $prepare->execute();
-		    
-            //念のため、更新数が1以上か確認
-            if ($emptyCheck > '')
+
+            //更新数が1以上か確認
+            if ($prepare->rowCount() > 0)
             {
                 $list .= '下記の注文を引き渡し完了にしました';
                 $list .= '<br><table style="width: 80vw; height: 2em;"><tr>';
