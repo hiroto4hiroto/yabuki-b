65a41506c9b6e6f6c3a69257b06ec1bb7d104799
takuyayamada <39893033+0105takuyayamada@users.noreply.github.com>
2019-07-11 00:14:07 +0900

    Update VQRreador.php

--- htdocs/VQRreador.php
+++ htdocs/VQRreador.php
@@ -11,20 +11,66 @@ if (!isset($_SESSION['VENDER'])) {
         $db = new PDO($dsn, $dbUser, $dbPass);
         
 	//引き渡し処理
-        if( !empty($_GET['QRid']) ){
-            //SQL作成・実行
+        $isCheck = 0;
+        $QRid = "";
+        if( !empty($_GET['QRid']) || !empty($_POST['delivery']) ){
+            $isCheck = 1;
+
+            //引き渡し完了に更新
             $sql = 'UPDATE `ordertable` LEFT JOIN bentotable ON `ordertable`.id = bentotable.id';
-            $sql .= ' SET `ordertable`.check = 1 WHERE `ordertable`.QRid = "'. $_GET["QRid"] .'";';
+            if (!empty($_GET['QRid']) )
+                $sql .= ' SET `ordertable`.check = 1 WHERE `ordertable`.QRid = "'. $_GET["QRid"] .'";';
+            else
+                $sql .= ' SET `ordertable`.check = 1 WHERE `ordertable`.user = "'. $_POST["user"] .'" and bentotable.date = "'. $getdate .'";';
             $prepare = $db->prepare($sql);
             $prepare->execute();
-        }	    
-	if( !empty($_POST['delivery']) ){
-            //SQL作成・実行
-            $sql = 'UPDATE `ordertable` LEFT JOIN bentotable ON `ordertable`.id = bentotable.id';
-            $sql .= ' SET `ordertable`.check = 1 WHERE `ordertable`.user = "'. $_POST["user"] .'" and bentotable.date = "'. $getdate .'";';
+
+            $db = new PDO($dsn, $dbUser, $dbPass);
+            //引き渡し完了に更新
+            $sql = 'SELECT QRid FROM `ordertable` LEFT JOIN bentotable ON `ordertable`.id = bentotable.id';
+            if (!empty($_GET['QRid']) )
+                $sql .= ' SET `ordertable`.check = 1 WHERE `ordertable`.QRid = "'. $_GET["QRid"] .'" limit 1;';
+            else
+                $sql .= ' SET `ordertable`.check = 1 WHERE `ordertable`.user = "'. $_POST["user"] .'" and bentotable.date = "'. $getdate .'" limit 1;';
             $prepare = $db->prepare($sql);
             $prepare->execute();
+
+            $QRid = $prepare->fetch(PDO::FETCH_ASSOC);
         }
+	
+	    if ($isCheck){
+            //引き渡し完了一覧作成
+            //SQL作成・実行
+            $sql = "SELECT `order`.QRid as `QRid`, `order`.check as `check`, `bento`.date as `date`, `order`.user as `user`, `order`.id as `id`, `bento`.name as `name`";
+            $sql .= " FROM `ordertable` as `order` LEFT JOIN `bentotable` as `bento` ON `order`.id = `bento`.id";
+            $sql .= " ORDER BY `order`.check, `bento`.date, `order`.user, `order`.id;";
+            $sql .= " WHERE `ordertable`.QRid = '". $_GET["QRid"] ."';";
+            $prepare = $db->prepare($sql);
+            $prepare->execute();
+            
+            $list .= '下記の注文を引き渡し完了にしました';
+            $list .= '<br><table style="width: 80vw; height: 2em;"><tr>';
+            $list .= '<td style="width: 5vw;">受取';
+            $list .= '<td style="width: 10vw;">日付';
+            $list .= '<td style="width: 10vw;">学生番号';
+            $list .= '<td style="width: 20vw;">弁当名';
+            //$list .= '<td style="width: 35vw;">UUID';
+            foreach ($prepare->fetchAll(PDO::FETCH_ASSOC) as $result)
+            {
+                $plusClass = '';
+                if ($result["date"] == $getdate) $plusClass = ' class="todayOrder" ';
+
+                $list .= '<tr>';
+                if ($result["check"] == 1)
+                    $list .= '<td'. $plusClass .' style="color:blue;">完了';
+                else $list .= '<td'. $plusClass .' style="color:red;">未了';
+                $list .= '<td'. $plusClass .'>'. $result["date"];
+                $list .= '<td'. $plusClass .'>'. $result["user"];
+                $list .= '<td'. $plusClass .'>'. $result["name"];
+            }
+            $list .= '</table>';
+        }
+        
         
         //予約一覧作成
         //SQL作成・実行
@@ -60,53 +106,54 @@ if (!isset($_SESSION['VENDER'])) {
         die();
     }
 ?>
-    <!DOCTYPE html>
-    <html>
+<!DOCTYPE html>
+<html>
 
-    <head>
-        <meta charset="utf-8" />
-        <title>弁当事前予約サービス 引き渡し操作</title>
-        <link rel="stylesheet" type="text/css" href="style.css">
-    </head>
+<head>
+    <meta charset="utf-8" />
+    <title>弁当事前予約サービス 引き渡し操作</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
 
-    <body class="vender">
-        <p>弁当事前予約サービス</p>
-        <h1>引き渡し操作</h1>
-	<input type="button" class="btn-sticky" onclick="location.href='./Vindex.php'" value="トップページに戻る"><br><br>
+<body class="vender">
+    <p>弁当事前予約サービス</p>
+    <h1>引き渡し操作</h1>
+    <input type="button" class="btn-sticky" onclick="location.href='./Vindex.php'" value="トップページに戻る"><br><br>
     <table>
-	<tr><td colspan="3">
-	<label class="btn-sticky" style="height: calc(var(--fontRatio) * 7.5);"><br>QRコードを撮影&引渡<br>
-		<input style="display:none;"
-	       type="file" accept="image/*" capture="environment" onchange="openQRCamera(this);" /><br>
         <tr>
-	<td><label style="width: 15vw;" for="user">学生番号</label>
-        <td><input style="width: 25vw;" id="user" style="width: 25vw;" type="text" name="user" maxlength="7">
-	<td><input style="width: 40vw;" class="btn-sticky" type="submit" name="delivery" value="引き渡し">
-    </table> 
-</form>
-</label> 
+            <td colspan="3">
+                <label class="btn-sticky" style="height: calc(var(--fontRatio) * 7.5);"><br>QRコードを撮影&引渡<br>
+                    <input style="display:none;" type="file" accept="image/*" capture="environment"
+                        onchange="openQRCamera(this);" /><br>
+        <tr>
+            <td><label style="width: 15vw;" for="user">学生番号</label>
+            <td><input style="width: 25vw;" id="user" style="width: 25vw;" type="text" name="user" maxlength="7">
+            <td><input style="width: 40vw;" class="btn-sticky" type="submit" name="delivery" value="引き渡し">
+    </table>
+    </form>
+    </label>
 
-<script type="text/javascript" charset="utf-8">
-function openQRCamera(node) {
-  var reader = new FileReader();
-  reader.onload = function() {
-    node.value = "";
-    qrcode.callback = function(res) {
-      if(res instanceof Error) {
-        alert("QRコードを認識できませんでした。");
-      } else {
-	//QR読み込み成功
-	window.location.href =　location.href + '?QRid=' + res;
-      }
-    };
-    qrcode.decode(reader.result);
-  };
-  reader.readAsDataURL(node.files[0]);
-}
-</script>
-        
-        <br>
-        <?php echo $list; ?>     
-    </body>
+    <script type="text/javascript" charset="utf-8">
+        function openQRCamera(node) {
+            var reader = new FileReader();
+            reader.onload = function () {
+                node.value = "";
+                qrcode.callback = function (res) {
+                    if (res instanceof Error) {
+                        alert("QRコードを認識できませんでした。");
+                    } else {
+                        //QR読み込み成功
+                        window.location.href = location.href + '?QRid=' + res;
+                    }
+                };
+                qrcode.decode(reader.result);
+            };
+            reader.readAsDataURL(node.files[0]);
+        }
+    </script>
 
-    </html>
+    <br>
+    <?php echo $list; ?>
+</body>
+
+</html>
