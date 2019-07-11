e74f5912c3bcc87a1debba264b56053459fafd0a
takuyayamada <39893033+0105takuyayamada@users.noreply.github.com>
2019-07-11 22:59:46 +0900

    Update VQRreador.php

--- htdocs/VQRreador.php
+++ htdocs/VQRreador.php
@@ -10,77 +10,81 @@ if (!isset($_SESSION['VENDER'])) {
         require_once 'database_conf.php';
         $db = new PDO($dsn, $dbUser, $dbPass);
         $db->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, true); 
-	    //引き渡し処理
-        $isCheck = 0;
-        $QRid = "";
+        //引き渡し処理
+        $check = 0;
         if( !empty($_GET['QRid']) || isset($_POST['delivery']) ){
 
-            //引き渡し完了に更新
-            $sql = 'UPDATE `ordertable` LEFT JOIN bentotable ON `ordertable`.id = bentotable.id';
+            //引き渡ししていない予約があるか
+            $sql = 'SELECT * FROM `ordertable` LEFT JOIN bentotable ON `ordertable`.id = bentotable.id';
             if (!empty($_GET['QRid']) )
-                $sql .= ' SET `ordertable`.check = 1 WHERE `ordertable`.check = 0 `ordertable`.QRid = "'. $_GET["QRid"] .'";';
+                $sql .= ' WHERE `ordertable`.check = 0 AND `ordertable`.QRid = "'. $_GET["QRid"] .'";';
             else
-                $sql .= ' SET `ordertable`.check = 1 WHERE `ordertable`.check = 0 `ordertable`.user = "'. $_POST["user"] .'" and bentotable.date = "'. $getdate .'";';
+                $sql .= ' WHERE `ordertable`.check = 0 AND `ordertable`.user = "'. $_POST["user"] .'" AND bentotable.date = "'. $getdate .'";';
             $prepare = $db->prepare($sql);
             $prepare->execute();
             foreach ($prepare->fetchAll(PDO::FETCH_ASSOC) as $result) {
-                $isCheck = 1;
+                $check = 1;
                 break;
             }
-            
-            $db = new PDO($dsn, $dbUser, $dbPass);
-            
-            //表示するレコードのQRidを設定
-            if (!empty($_GET['QRid'])) $QRid = $_GET['QRid'];
-            else {
-                $sql = 'SELECT QRid FROM `ordertable` LEFT JOIN bentotable ON `ordertable`.id = bentotable.id';
-                $sql .= ' WHERE `ordertable`.user = "'. $_POST["user"] .'" and bentotable.date = "'. $getdate .'" limit 1;';
+
+            if ($check == 1){
+                //引き渡し完了に更新
+                $sql = 'UPDATE `ordertable` LEFT JOIN bentotable ON `ordertable`.id = bentotable.id';
+                if (!empty($_GET['QRid']) )
+                    $sql .= ' SET `ordertable`.check = 1 WHERE `ordertable`.check = 0 `ordertable`.QRid = "'. $_GET["QRid"] .'";';
+                else
+                    $sql .= ' SET `ordertable`.check = 1 WHERE `ordertable`.check = 0 `ordertable`.user = "'. $_POST["user"] .'" and bentotable.date = "'. $getdate .'";';
                 $prepare = $db->prepare($sql);
                 $prepare->execute();
-                $QRid = $prepare->fetch(PDO::FETCH_ASSOC)["QRid"];
-            }
-        }
-	
-	    if ($isCheck == 1){
-            //引き渡し完了一覧作成
-            //SQL作成・実行
-            $sql = "SELECT `order`.QRid as `QRid`, `order`.check as `check`, `bento`.price as `price`, `order`.user as `user`, `order`.id as `id`, `bento`.name as `name`";
-            $sql .= " FROM `ordertable` as `order` LEFT JOIN `bentotable` as `bento` ON `order`.id = `bento`.id";
-            $sql .= " WHERE `order`.QRid = '". $QRid ."'";
-            $sql .= " ORDER BY `order`.check, `bento`.date, `order`.user, `order`.id;";
-            $prepare = $db->prepare($sql);
-	        $prepare->execute();
 
-	        $tempList = "";
-            $tempList .= '下記の注文を引き渡し完了にしました。';
-            $tempList .= '<br><table style="width: 80vw; height: 2em;"><tr>';
-            $tempList .= '<td style="width: 10vw;">受取';
-            $tempList .= '<td style="width: 20vw;">学生番号';
-            $tempList .= '<td style="width: 25vw;"><b>弁当名</b>';
-	        $tempList .= '<td style="width: 15vw;">値段';
-	        $tempForeachList = "";
-	        $sum = 0;
-            foreach ($prepare->fetchAll(PDO::FETCH_ASSOC) as $result)
-            {
-                $tempForeachList .= '<tr>';
-                $tempForeachList .= '<td class="todayOrder" style="color:blue;">完了';
-                $tempForeachList .= '<td class="todayOrder">'. $result["user"];
-                $tempForeachList .= '<td class="todayOrder">'. $result["name"];
-	            $tempForeachList .= '<td class="todayOrder">'. $result["price"].'円';
-	            $sum += $result["price"];
-            }
+                $db = new PDO($dsn, $dbUser, $dbPass);
 
-            //foreach文を通ったら代入
-            if ($tempForeachList != ""){
+                //表示するレコードのQRidを設定
+                $QRid = "";
+                if (!empty($_GET['QRid'])) $QRid = $_GET['QRid'];
+                else {
+                    $sql = 'SELECT QRid FROM `ordertable` LEFT JOIN bentotable ON `ordertable`.id = bentotable.id';
+                    $sql .= ' WHERE `ordertable`.user = "'. $_POST["user"] .'" and bentotable.date = "'. $getdate .'" limit 1;';
+                    $prepare = $db->prepare($sql);
+                    $prepare->execute();
+                    $QRid = $prepare->fetch(PDO::FETCH_ASSOC)["QRid"];
+                }
+
+                //引き渡し完了一覧作成
+                //SQL作成・実行
+                $sql = "SELECT `order`.QRid as `QRid`, `order`.check as `check`, `bento`.price as `price`, `order`.user as `user`, `order`.id as `id`, `bento`.name as `name`";
+                $sql .= " FROM `ordertable` as `order` LEFT JOIN `bentotable` as `bento` ON `order`.id = `bento`.id";
+                $sql .= " WHERE `order`.QRid = '". $QRid ."'";
+                $sql .= " ORDER BY `order`.check, `bento`.date, `order`.user, `order`.id;";
+                $prepare = $db->prepare($sql);
+	            $prepare->execute();
+
+	            $tempList = "";
+                $tempList .= '下記の注文を引き渡し完了にしました。';
+                $tempList .= '<br><table style="width: 80vw; height: 2em;"><tr>';
+                $tempList .= '<td style="width: 10vw;">受取';
+                $tempList .= '<td style="width: 20vw;">学生番号';
+                $tempList .= '<td style="width: 25vw;"><b>弁当名</b>';
+	            $tempList .= '<td style="width: 15vw;">値段';
+	            $tempForeachList = "";
+	            $sum = 0;
+                foreach ($prepare->fetchAll(PDO::FETCH_ASSOC) as $result)
+                {
+                    $tempForeachList .= '<tr>';
+                    $tempForeachList .= '<td class="todayOrder" style="color:blue;">完了';
+                    $tempForeachList .= '<td class="todayOrder">'. $result["user"];
+                    $tempForeachList .= '<td class="todayOrder">'. $result["name"];
+	                $tempForeachList .= '<td class="todayOrder">'. $result["price"].'円';
+	                $sum += $result["price"];
+                }
                 $list .= $tempList;
                 $list .= $tempForeachList;
-	            $list .= '<tr><td colspan="4">合計金額：<b style="font-size: calc(var(--fontRatio) * 2);">'. $sum .'</b>円</table>';
+                $list .= '<tr><td colspan="4">合計金額：<b style="font-size: calc(var(--fontRatio) * 2);">'. $sum .'</b>円</table>';
             }
-            else
-            {   //foreach文を通っていなかったらエラー文
-                $list .= '入力に対応する未了予約がありませんでした。<br>入力内容が正しいか確認してください。<br>';
+            else{
+                $list .= '入力に対応する未了予約がありませんでした。<br>入力内容が正しいかご確認ください。<br>';
             }
-        }   
+        } 
 
         //予約一覧作成
         //SQL作成・実行
